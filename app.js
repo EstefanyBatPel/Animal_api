a64aff1ce6c1a1809b3051d45a48c72f30be20c5
mariandrean <m.andreaan@gmail.com>
2024-03-05 14:50:52 +0100

    adding express port to app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+import { PORT } from "./config.js";
 import connection_db from "./database/connection_db.js";
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola, esta es una Api básica');
+});
 
 try {
-    await connection_db.authenticate();
-    console.log('We are in conection ( /^u^)/ Yipiee!!');
-  } catch (error) {
-    console.error('Oh oh...We have a problem ( ono) error:', error);
-  }
+  await connection_db.authenticate();
+  console.log('We are in conection ( /^u^)/ Yipiee!!');
+} catch (error) {
+  console.error('Oh oh...We have a problem ( ono) error:', error);
+}
+
+app.listen(PORT, () => {
+  console.log(`Escuchando en el puerto ${PORT}`);
+});
--- config.js
+++ config.js
@@ -3,3 +3,4 @@ import 'dotenv/config';
 export const DB_NAME = process.env.DATABASE; 
 export const DB_USERNAME = process.env.USER_NAME;  
 export const DB_PASSWORD = process.env.SECRET_KEY; 
+export const PORT = process.env.PORT || 5000;
