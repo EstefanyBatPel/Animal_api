c2d98b6d1a27117de89276dea21129955195a4df
Andreamartinn17 <andreamartinn17@gmail.com>
2024-03-05 12:13:40 +0100

    sequelize model

---
+++ models/AnimalModel.js
@@ -0,0 +1,51 @@
+import { DataTypes } from 'sequelize';
+import connection_db from '../database/connection_db.js';
+
+// Definir el modelo Animal
+const animal = connection_db.define('animal', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  scientificName: {
+    type: DataTypes.STRING(80),
+    allowNull: false
+  },
+  image: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  photographer: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  sound: {
+    type: DataTypes.STRING(100)
+  },
+  description: {
+    type: DataTypes.STRING(200),
+    allowNull: false
+  }
+}, {
+  // Opciones adicionales del modelo
+  tableName: 'animals', // Nombre de la tabla en la base de datos
+  timestamps: false // Deshabilitar los campos createdAt y updatedAt
+});
+
+// Sincronizar el modelo con la base de datos
+(async () => {
+  try {
+    await connection_db.sync();
+    console.log('Model and database synchronized correctly');
+  } catch (error) {
+    console.error('Synchronization error:', error);
+  }
+})();
+
+console.log(animal)
+console.log(animal === connection_db.models.animal); // true
