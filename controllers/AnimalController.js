dde31a96760c571e1dbdf3d3647620f392e43e2a
Andreamartinn17 <andreamartinn17@gmail.com>
2024-03-08 11:10:34 +0100

    delete method

--- controllers/AnimalController.js
+++ controllers/AnimalController.js
@@ -12,3 +12,15 @@ export const getAllAnimals = async (request, response) => {
     }
 
 }
+
+export const deleteAnimal = async (request, response) => {
+    
+    try {
+        const { id } = request.params;
+        const animal = await AnimalModel.destroy({ where: { id }});
+        response.status(200).json(animal);
+    } 
+    catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+}
--- routes/AnimalRouter.js
+++ routes/AnimalRouter.js
@@ -1,8 +1,11 @@
 import express from "express";
 import {getAllAnimals} from "../controllers/AnimalController.js"
+import { deleteAnimal } from "../controllers/AnimalController.js"
 
 const router = express.Router();
 
 router.get("/", getAllAnimals);
+
+router.delete("/:id", deleteAnimal);
 
 export default router;
