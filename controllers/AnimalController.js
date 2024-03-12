14e658338537212a512a55499554a120870e2103
EstefanyBatPel <estefanybatpel@gmail.com>
2024-03-12 14:38:05 +0100

    validations

--- controllers/AnimalController.js
+++ controllers/AnimalController.js
@@ -1,6 +1,5 @@
 import AnimalModel from "../models/AnimalModel.js"
 
-
 export const getAllAnimals = async (request, response) => {
     
     try {
@@ -39,18 +38,35 @@ export const deleteAnimal = async (request, response) => {
     }
 }
 
-export const createAnimal = async (request, response) => {
-    try {
-        
-        // Crear un nuevo animal utilizando el modelo AnimalModel y los datos proporcionados
-        const newAnimal = await AnimalModel.create(request.body);
 
-        // Responder con un estado 201 (Creado) y el nuevo animal creado
-        response.status(201).json(newAnimal);
-    } catch (error) {
-        // Si ocurre algún error, responder con un estado 500 (Error interno del servidor) y un mensaje de error
-        response.status(500).json({ message: error.message });
+export const createAnimal = async (request, response) => {
+
+    //tutorial
+
+    try{
+        const {name, scientificName, image, photographer, sound, description} = request.body;
+        const resDetail = await AnimalModel.create({
+            name, scientificName, image, photographer, sound, description
+        })
+        response.send({data: resDetail})
+    }catch(error){
+        httpError(response,error)
     }
+
+    //fin tutorial
+
+
+    // try {
+
+    //     // Crear un nuevo animal utilizando el modelo AnimalModel y los datos proporcionados
+    //     const newAnimal = await AnimalModel.create(request.body);
+
+    //     // Responder con un estado 201 (Creado) y el nuevo animal creado
+    //     response.status(201).json(newAnimal);
+    // } catch (error) {
+    //     // Si ocurre algún error, responder con un estado 500 (Error interno del servidor) y un mensaje de error
+    //     response.status(500).json({ message: error.message });
+    // }
 };
 
 export const updatedAnimal = async (request, response) => {
--- routes/AnimalRouter.js
+++ routes/AnimalRouter.js
@@ -1,15 +1,53 @@
-import express from "express";
+import express, { query } from "express";
 import {getAllAnimals, updatedAnimal, deleteAnimal, getAnimalById, createAnimal} from "../controllers/AnimalController.js"
+// const {validateCreate} = require('../validators/animals')
+import { validateCreate } from "../validators/animals.js";
+import {validationResult} from 'express-validator'
+import {check} from 'express-validator'
 
 const router = express.Router();
 
-router.get("/", getAllAnimals);
+router.get('/',  [
+    check('name')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('scientificName')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('photographer')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('image')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('sound')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('description')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+  ],
+    (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  
+    getAllAnimals(req, res);
+  });
+
+// router.get("/", getAllAnimals);
 
 router.get('/:id', getAnimalById);
 
 router.delete("/:id", deleteAnimal);
 
-router.post("/", createAnimal);
+router.post("/", validateCreate, createAnimal);
 
 router.put("/:id", updatedAnimal)
 
