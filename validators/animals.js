6c5fdfcdb09708cd2c8329391af49fd053d12b51
EstefanyBatPel <estefanybatpel@gmail.com>
2024-03-13 10:48:11 +0100

    comentary

--- validators/animals.js
+++ validators/animals.js
@@ -1,4 +1,4 @@
-import {check} from 'express-validator'
+import {check, body} from 'express-validator' //se puede hacer con check y con body
 import { validateResult } from '../helpers/validateHelper.js';
 
 // Reglas de validación al obtener los datos de la api
