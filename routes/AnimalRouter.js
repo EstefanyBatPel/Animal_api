b12f9abb34a239f57fa50c7ae3f2ef9e8f25441b
EstefanyBatPel <estefanybatpel@gmail.com>
2024-03-13 09:36:11 +0100

    order getvalidation in the correct folder

--- routes/AnimalRouter.js
+++ routes/AnimalRouter.js
@@ -1,47 +1,10 @@
-import express, { query } from "express";
+import express from "express";
 import {getAllAnimals, updatedAnimal, deleteAnimal, getAnimalById, createAnimal} from "../controllers/AnimalController.js"
-// const {validateCreate} = require('../validators/animals')
-import { validateCreate } from "../validators/animals.js";
-import {validationResult} from 'express-validator'
-import {check} from 'express-validator'
+import { validateCreate, validateGet } from "../validators/animals.js";
 
 const router = express.Router();
 
-router.get('/',  [
-    check('name')
-      .isString()
-      .withMessage('El nombre debe tener solo letras'),
-
-    check('scientificName')
-      .isString()
-      .withMessage('El nombre debe tener solo letras'),
-
-    check('photographer')
-      .isString()
-      .withMessage('El nombre debe tener solo letras'),
-
-    check('image')
-      .isString()
-      .withMessage('El nombre debe tener solo letras'),
-
-    check('sound')
-      .isString()
-      .withMessage('El nombre debe tener solo letras'),
-
-    check('description')
-      .isString()
-      .withMessage('El nombre debe tener solo letras'),
-  ],
-    (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-  
-    getAllAnimals(req, res);
-  });
-
-// router.get("/", getAllAnimals);
+router.get("/", validateGet, getAllAnimals);
 
 router.get('/:id', getAnimalById);
 
--- validators/animals.js
+++ validators/animals.js
@@ -1,6 +1,37 @@
 import {check} from 'express-validator'
 import { validateResult } from '../helpers/validateHelper.js';
 
+// Reglas de validación al obtener los datos de la api
+export const validateGet = [
+    check('name')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('scientificName')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('photographer')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('image')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('sound')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    check('description')
+      .isString()
+      .withMessage('El nombre debe tener solo letras'),
+
+    (req, res, next) => {
+        validateResult(req, res, next)
+    }
+  ];
+
 // Reglas de validación para la creación de un animal
 export const validateCreate = [
     check('name')
