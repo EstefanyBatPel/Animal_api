6a80e0ca20f4c23ebd702dfb545abbfd5ad6e56b
mariandrean <m.andreaan@gmail.com>
2024-03-18 11:54:35 +0100

    unifying create and update animal validators

--- validators/AnimalValidators.js
+++ validators/AnimalValidators.js
@@ -3,60 +3,76 @@ import AnimalModel from "../models/AnimalModel.js";
 import { validateResult } from "../helpers/validateHelper.js";
 
 export const validateCreateAnimal = [
-  check('name')
-      .exists()
-      .notEmpty(),
+    check('name')
+        .exists()
+        .notEmpty().withMessage('The name of the animal is required')
+        .isLength({ min: 1 }).withMessage('The name must be at least 5 characters'),
 
-  check('scientificName')
-      .exists()
-      .notEmpty(),
+    check('scientificName')
+        .exists()
+        .notEmpty()
+        .isAlpha().withMessage('They must be alphabetical characters'),
 
-  check('photographer')
-      .exists()
-      .notEmpty(),
+    check('photographer')
+        .exists()
+        .notEmpty().withMessage('The photographer is required'),
 
-  check('image')
-      .exists()
-      .notEmpty(),
+    check('sound')
+        .exists()
+        .notEmpty().withMessage('Sound is required')
+        .isURL().withMessage('Sound must be a valid URL'),
 
-  check('sound')
-      .exists()
-      .notEmpty(),
+    check('description')
+        .exists()
+        .notEmpty().withMessage('Description is required')
+        .isLength({ min: 5 }).withMessage('The description must be at least 5 characters')
+        .isLength({ max: 200 }).withMessage('The name must have a maximum of 200 characters'),
 
-  check('description')
-      .exists()
-      .notEmpty()
-      .isLength({ min: 5 }).withMessage('The description must be at least 5 characters'),
-
-  (req, res, next) => {
-      validateResult(req, res, next)
-  }
+    (req, res, next) => {
+        validateResult(req, res, next)
+    }
 ];
 
 export const validateUpdateAnimal = [
+    param("id")
+        .exists()
+        .notEmpty()
+        .custom(async id => {
+            const task = await AnimalModel.findByPk(id);
+            if (!task) {
+                throw new Error('The animal you are trying to modify does not exist');
+            }
+        }),
     check('name')
-      .notEmpty().withMessage('The name of the animal is required')
-      .isLength({ min: 5 }).withMessage('The name must be at least 5 characters'),
-  
-    check('scientificName').isAlpha().withMessage('They must be alphabetical characters'),
-  
-    check('image')
-      .notEmpty().withMessage('The image is required')
-      .isURL().withMessage('Image must be a valid URL'),
-  
-    check('photographer').notEmpty().withMessage('The photographer is required'),
-  
-    check('sound').isURL().withMessage('Sound must be a valid URL'),
-  
+        .exists()
+        .notEmpty().withMessage('The name of the animal is required')
+        .isLength({ min: 1 }).withMessage('The name must be at least 5 characters'),
+
+    check('scientificName')
+        .exists()
+        .notEmpty()
+        .isAlpha().withMessage('They must be alphabetical characters'),
+
+    check('photographer')
+        .exists()
+        .notEmpty().withMessage('The photographer is required'),
+
+    check('sound')
+        .exists()
+        .notEmpty().withMessage('Sound is required')
+        .isURL().withMessage('Sound must be a valid URL'),
+
     check('description')
-      .notEmpty().withMessage('Description is required')
-      .isLength({ min: 20 }).withMessage('Description must be at least 20 characters')
-      .isLength({ max: 200 }).withMessage('The name must have a maximum of 200 characters'),
-  
+        .exists()
+        .notEmpty().withMessage('Description is required')
+        .isLength({ min: 5 }).withMessage('The description must be at least 5 characters')
+        .isLength({ max: 200 }).withMessage('The name must have a maximum of 200 characters'),
+
+
     (request, response, next) => {
-      validateResult(request, response, next);
+        validateResult(request, response, next);
     }
-  ];
+];
 
 export const validateDeleteAnimal = [
     param("id")
