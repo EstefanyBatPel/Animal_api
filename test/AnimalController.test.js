065572e8d4b9ce450b4c6b14e9d80000f01516e1
mariandrean <m.andreaan@gmail.com>
2024-03-19 15:25:28 +0100

    modifying tests

--- test/AnimalController.test.js
+++ test/AnimalController.test.js
@@ -2,27 +2,35 @@ import { app } from '../app.js';
 import request from 'supertest'
 import connection_db from '../database/connection_db.js';
 import AnimalModel from '../models/AnimalModel.js';
+import { Op } from 'sequelize';
 
 const api = request(app);
-let animalTestData = {
-    "name": "test",
-    "image": "http://testing.com",
-    "scientificName": "test",
-    "photographer": "test",
-    "sound": "http://testing.com",
-    "description": "testing"
-}
+
 
 describe('Testing CRUD animals', () => {
 
+    let createdAnimalTest;
+    const animalTestData = {
+        "name": "test",
+        "image": "http://testing.com",
+        "scientificName": "test",
+        "photographer": "test",
+        "sound": "http://testing.com",
+        "description": "testing"
+    }
+
+    beforeEach(async () => {
+        createdAnimalTest = await api.post('/api').send(animalTestData);
+    })
+
     afterAll(async () => {
-        await AnimalModel.destroy({ where: { "name": "test" } });
+        await AnimalModel.destroy({ where: { "name": { [Op.startsWith]: 'test' } } });
         //await AnimalModel.truncate(); // Elimina todas las instancias de animales de la base de datos
         await connection_db.close();
     });
 
     describe('Testing Get Animals', () => {
-        test('Response body must an array and then show 200 status', async () => { 
+        test('Response body must an array and then show 200 status', async () => {
             const response = await api.get('/api');
             expect(Array.isArray(response.body)).toBe(true);
             //expect(response.body).toHaveLength(0);
@@ -39,20 +47,17 @@ describe('Testing CRUD animals', () => {
             const response = await api.post('/api').send(animalTestData);
             expect(typeof response.body).toBe('object');
         });
+        test('Should not create an animal if request body is empty and return status 403', async () => {
+            const response = await api.post('/api');
+            expect(response.status).toBe(403);
+        })
     })
     
+    
+    //Test del Update
+
     //Test del Update
     describe('Testing Put - Update Animal', () => {
-
-        let createdAnimalTest;
-
-        beforeAll(async () => {
-            createdAnimalTest = await api.post('/api').send(animalTestData);
-        })
-
-        afterAll(async () => {
-            await AnimalModel.destroy({ where: { "name": "test updated" } });
-        })
 
         test('Should update name in an animal and return status 200', async () => {
             animalTestData.name = "test updated"
@@ -69,51 +74,26 @@ describe('Testing CRUD animals', () => {
         })
     })
 
-    //Test del Delete
-
-    describe ('Testing delete animal', () => {
-
-        let createdAnimalTest;
-
-        beforeAll(async () => {
-            createdAnimalTest = await api.post('/api').send(animalTestData);
-        })
-
-        afterAll(async () => {
-            await AnimalModel.destroy({ where: { "name": "delete animal" } });
-        })
-        
-        test('Should deleted Animal', async() =>{
-        const response = await  api.delete(`/api/${createdAnimalTest.body.id}`).send({
-            "name": "delete animal",
-            });
-            expect(response.status).toBe(200);
-        })
-
-        test('Should return 500 if attempting to delete a non-existing animal', async () =>{
-            const response = await api.delete(`/api/${createdAnimalTest.body.id}`);
-            expect(response.status).toBe(403);
-        })
-
-    });
-
-
-    //Test del Get One
-
-    describe ('Testing Get One animal', () => {
-
-        let createdAnimalTest;
-
-        beforeAll(async () => {
-            createdAnimalTest = await api.post('/api').send(animalTestData);
-        })
-
-        test('Should get one animal and return status 200', async () =>{
+    describe('Testing Get One Animal', () => {
+        test('Should get one animal and return status 200', async () => {
             const response = await api.get(`/api/${createdAnimalTest.body.id}`);
             expect(response.status).toBe(200);
         })
     });
 
-    // Test extra ? 
+    describe('Testing delete animal', () => {
+
+        test('Should deleted Animal and return status 200', async () => {
+            const response = await api.delete(`/api/${createdAnimalTest.body.id}`);
+            expect(response.status).toBe(200);
+        })
+
+        test('Should return 500 if attempting to delete a non-existing animal', async () => {
+            await api.delete(`/api/${createdAnimalTest.body.id}`);
+            const response = await api.delete(`/api/${createdAnimalTest.body.id}`);
+            expect(response.status).toBe(403);
+        })
+
+    });
 
 });
